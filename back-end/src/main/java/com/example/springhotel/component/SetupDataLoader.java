b12660058a9f380fd8999924ede4bc263d34b4eb
azerkane <benchrifmohamed79@gmail.com>
2026-01-05 16:17:35 +0100

    Structuration-projet

--- back-end/src/main/java/com/example/springhotel/component/SetupDataLoader.java
+++ back-end/src/main/java/com/example/springhotel/component/SetupDataLoader.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
 
 import jakarta.transaction.Transactional;
 import java.util.Arrays;
